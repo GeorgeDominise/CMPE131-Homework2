1ad9d3b5586fe60b73af686434074976bcd4b81d
GeorgeDominise <99700684+GeorgeDominise@users.noreply.github.com>
2022-03-10 23:31:38 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,13 +1,17 @@
 def sort_list(input):
     n = len(input)
-    i = 0   
+    i = 1   
     while (i<n):
-        j = i + 1
-        while(j < n-i-1):
-            if(input[i] > input[j]):
+        j = 0
+        while(j <= n-i-1):
+            if(input[j+1] < input[j]):
                 temp = input[i]
                 input[i] = input[j]
                 input[j] = temp
             j = j + 1
         i = i + 1
     return input
+
+input = [21, 54, 63, 11, 31, 44]
+sort_list(input)
+print(input)
